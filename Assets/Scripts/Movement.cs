a1de7959dc370d12a00c9deffd0db3af4bb2fe17
Darkon-the-Overlord <pollarddalon98@gmail.com>
2017-01-17 14:57:10 -0500

    Test

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,8 +7,29 @@ namespace Assets.Scripts
     public class Movement
     {
         private Panel[,] area = new Panel[6,3];
+        private int areaX = 0, areaY = 0;
+        
+        void Update()
+        {
+            
+            if (Input.GetKey(KeyCode.W))
+            {
+                areaY++;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                areaY--;
+            }
 
-
+            if (Input.GetKey(KeyCode.D))
+            {
+                areaX++;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                areaX--;
+            }
+        }
     }
 }
 
